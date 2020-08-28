5420abf0f7a13cd02634a86b13d0b346135dd61d
9631kunn <infourbh+1@gmail.com>
2020-08-28 14:27:41 +0900

    fix: button design

--- src/components/GoToTop.js
+++ src/components/GoToTop.js
@@ -3,18 +3,31 @@ import scrollTo from 'gatsby-plugin-smoothscroll';
 
 import styled from "styled-components"
 
+import UpIcon from "../icon/up-arrow.svg"
+
+const mediaS = props => props.theme.media.s
+const mediaL = props => props.theme.media.l
+
 const Wrap = styled.div`
   bottom: 10px;
   position: fixed;
-  right: 15vw;
+  right: 5vw;
+  @media (min-width: ${mediaS}){
+    right: 10vw;
+  }
+  @media (min-width: ${mediaL}){
+    right: 15vw;
+  }
   button::before{
-    content: "↑";
+    background-image: url(${UpIcon});
+    content: "";
     display: inline-block;
-    height: 20px;
-    width: 20px;
+    height: 30px;
+    opacity: 0.5;
+    width: 30px;
   }
 `
 
-const GoToTop = () => <Wrap><button onClick={() => scrollTo('#header')} /></Wrap>
+const GoToTop = () => <Wrap><button onClick={() => scrollTo('#header')} aria-label="button" /></Wrap>
 
 export default GoToTop
